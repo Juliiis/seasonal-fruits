3567cfab2b87bd245fa4780f761d1c9a525676b8
julieta <jumartinnob@gmail.com>
2020-12-10 19:45:59 +0100

    added dummy routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import list from "./Components/list";
+import Description from "./Components/description";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/">Seasonal Fruits</Link>
+        </nav>
+        <ul>
+          <li>
+            <Link to="/list">List</Link>
+          </li>
+          <li>
+            <Link to="/description">Description</Link>
+          </li>
+        </ul>
+      </div>
+      <Switch>
+        <Route path="/list" component={list}></Route>
+        <Route path="/description" component={Description} />
+        <Route path="/" component={list}></Route>
+      </Switch>
+    </Router >
   );
 }
 
---
+++ src/Components/list.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class list extends Component {
+    render() {
+        return (
+            <div>
+                <h2>I'm a list</h2>
+            </div>
+        )
+    }
+}
