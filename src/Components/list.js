722c4df0bb6be24e4a6407a0a5bfd3adbe5b9e02
Julieta <jumartinnob@gmail.com>
2021-01-19 21:08:04 +0100

    connected with backend

--- src/Components/list.js
+++ src/Components/list.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const backendResponse = [
-    {
-      "id": "grape",
-      "name": "Grape",
-      "monthStart": 9,
-      "monthEnd": 1,
-      "properties": [
-        "You can make wine with them!"
-      ]
-    }
-  ]
+const currentMonth = (new Date()).getMonth() + 1
 
 function List() {
+    const [fruits, setFruits] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [month, setMonth] = useState(currentMonth)
+
+    useEffect(async () => {
+        const response = await fetch(`/api/available-fruits?month=${month}`)
+        setFruits(await response.json())
+        setLoading(false)
+    }, [month])
+
+    if (loading) {
+        return 'Loading'
+    }
+
     return (
         <div>
             <h2>List</h2>
             <ul>
-                {backendResponse.map(fruit => (
-                    <li>
+                {fruits.map(fruit => (
+                    <li key={fruit.id}>
                         <Link to={`/description/${fruit.id}`}>
                             {fruit.name}
                         </Link>
