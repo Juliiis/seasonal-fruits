1301d6bbba8ba9cd3d53de41258291497fe39bbb
Julieta <jumartinnob@gmail.com>
2021-01-19 19:49:15 +0100

    prepare list for backend response

--- src/Components/list.js
+++ src/Components/list.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const backendResponse = [
+    {
+      "id": "grape",
+      "name": "Grape",
+      "monthStart": 9,
+      "monthEnd": 1,
+      "properties": [
+        "You can make wine with them!"
+      ]
+    }
+  ]
+
 function List() {
     return (
         <div>
             <h2>List</h2>
             <ul>
-                <li>
-                    <Link to="/description/banana">Banana</Link>
-                </li>
-                <li>
-                    <Link to="/description/pera">Pera</Link>
-                </li>
-                <li>
-                    <Link to="/description/manzana">Manzana</Link>
-                </li>
+                {backendResponse.map(fruit => (
+                    <li>
+                        <Link to={`/description/${fruit.id}`}>
+                            {fruit.name}
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </div>
     )
